913c32e3d99c0ebb16f7b67f786adf523a05d47c
PomboObeso <matheus.gomes789@gmail.com>
2019-12-02 23:39:05 -0300

    Third Update

--- Farmacia/src/application/Manager.java
+++ Farmacia/src/application/Manager.java
@@ -7,6 +7,7 @@ import entities.Estoque;
 import entities.Remedio;
 import entities.Receita;
 import operador.Validator;
+import java.lang.ArrayIndexOutOfBoundsException;
 
 public class Manager{
     public static final int maxQtd = 5;
@@ -14,22 +15,44 @@ public class Manager{
     Validator validador = new Validator();
     Scanner input = new Scanner(System.in);
     
-    String id, nome, cpf;
+    String id, name, cpf;
     public void addOperador(){
-        System.out.println("Digite o ID do Operador");
-        id = input.nextLine();
-        System.out.println("Digite o nome do Operador");
-        nome = input.nextLine();
-        System.out.println("DIgite o CPf do Operador");
-        cpf = input.nextLine();
-        boolean retorno = validador.Validator(cpf);
-        if(retorno = true){
-            System.out.println("CPF aceito com sucesso");
-        }else{
-            System.out.println("CPF inválido! Tente novo");
-            addOperador();
+        try{
+            System.out.println("Digite o ID do Operador");
+            id = input.nextLine();
+            System.out.println("Digite o nome do Operador");
+            name = input.nextLine();
+            System.out.println("DIgite o CPF do Operador");
+            cpf = input.nextLine();
+            boolean retorno = validador.Validator(cpf);
+            if(retorno = true){
+                System.out.println("CPF aceito com sucesso");
+                int free = getFreeId();
+                if(free != -1){
+                    operador[free] = new Operador(id, name, cpf); 
+                }
+                else{
+                    System.out.println("Não há ID's Disponíveis!");
+                }
+            }else{
+                System.out.println("Cpf invalido digite novamente o CPF");
+                addOperador();    
+            }
+        }catch(ArrayIndexOutOfBoundsException erro){
+           System.out.println("Quantidade Máxima de Operadores alcançada!"); 
         }
-               
+   
+    }
+    public int getFreeId(){
+        for(int j = 0; j < maxQtd; j++){
+            if(operador[j] == null){
+                return j;
+            }
+        }
+            return -1;        
+    }
+    
+    public void alterarOperador(){
         
     }
 }
